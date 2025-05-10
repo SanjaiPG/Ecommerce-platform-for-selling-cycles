cc3df6cf0fc00f474eeefa3f33bc7bf667c469c8
SanjaiPG <sanjaigukesan8686@gmail.com>
2025-05-10 18:01:19 +0530

    Changes in design

--- Back-End/ecom/skyraptor/urls.py
+++ Back-End/ecom/skyraptor/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- Back-End/ecom/skyraptor/views.py
+++ Back-End/ecom/skyraptor/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     products = Product.objects.all()
@@ -9,5 +11,8 @@ def products(request):
     products = Product.objects.all()
     return render(request, 'html/products.html', {'products': products})
 
-def login(request):
+def login_user(request):
     return render(request, 'html/login.html', {})
+
+def logout_user(request):
+    return render(request, 'html/home.html', {})
