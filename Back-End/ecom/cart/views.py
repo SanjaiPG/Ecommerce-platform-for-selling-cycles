0c579b574fcf01f4909ac983683e38f5c4f3892b
SanjaiPG <sanjaigukesan8686@gmail.com>
2025-05-18 09:39:25 +0530

    Added product to cart

--- Back-End/ecom/cart/views.py
+++ Back-End/ecom/cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from skyraptor.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -6,7 +9,14 @@ def cart_summary(request):
     return render(request, 'html/cart_summary.html', {})
 
 def cart_add(request):
-    return render(request, 'html/cart_add.html', {})
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        Product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=Product_id)
+
+        cart.add(product=product)
+        response = JsonResponse({'Product Name: ': product.name})
+        return response
 
 def cart_remove(request):
     return render(request, 'html/cart_remove.html', {})
