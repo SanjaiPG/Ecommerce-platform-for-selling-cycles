9e71ad0fd43077f3f73c5848dadea416bfae11b5
SanjaiPG <sanjaigukesan8686@gmail.com>
2025-06-11 23:39:01 +0530

    Admin area changes

--- Back-End/ecom/payment/models.py
+++ Back-End/ecom/payment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=13)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    pin_code = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'Shipping Address'
+        verbose_name_plural = 'Shipping Addresses'
+
+    def __str__(self):
+        return f'shipping address - {str(self.id)}'
--- Back-End/ecom/skyraptor/admin.py
+++ Back-End/ecom/skyraptor/admin.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
-from .models import Category, Product, Customer, Order, Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from .models import Category, Product, Customer, Order, Profile
 
+# Register models normally
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(Profile)
 
+# Inline for Profile model
 class ProfileInline(admin.StackedInline):
     model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
 
-class UserAdmin(admin.ModelAdmin):
-    model = User
-    fields = ('username', 'email', 'first_name', 'last_name')
-    inlines = [ProfileInline]
+# Extend default UserAdmin and include Profile inline
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
 
+# Unregister default User admin and register customized one
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
