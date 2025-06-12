fba76422861cc716c5d0b470435e6b60b4f8029c
SanjaiPG <sanjaigukesan8686@gmail.com>
2025-06-12 23:06:43 +0530

    Chekout page with dynamic changes

--- Back-End/ecom/payment/views.py
+++ Back-End/ecom/payment/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from cart.cart import Cart
+from payment.forms import ShippingAddressForm
+from payment.models import ShippingAddress
+
 
 # Create your views here.
 
@@ -7,4 +11,12 @@ def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
 
 def checkout(request):
-    return render(request, 'payment/checkout.html', {})
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    quantities = cart.get_quants()
+    total = cart.get_total()
+
+    if request.user.is_authenticated:
+        shipping_user = ShippingAddress.objects.get(id=request.user.id)
+        shipping_form = ShippingAddressForm(request.POST or None, instance=shipping_user)
+        return render(request, 'payment/checkout.html', {"cart_products": cart_products, "quantities": quantities, "total": total, "shipping_form": shipping_form})
