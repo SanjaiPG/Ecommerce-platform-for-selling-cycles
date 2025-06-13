bb684a6eaf7a703775288c3a7c5ce42757bbec29
SanjaiPG <sanjaigukesan8686@gmail.com>
2025-06-14 00:20:32 +0530

    Order inline items

--- Back-End/ecom/payment/admin.py
+++ Back-End/ecom/payment/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+class orderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ('date_ordered',)
+    inlines = [orderItemInline]
+
+admin.site.unregister(Order)
+
+admin.site.register(Order, OrderAdmin)
